05ba33ebd4aedd341962edaee03c0bdf845d0628
mathewsamuel04 <mathewsamuel19524@gmail.com>
2025-07-24 09:34:06 +0530

    Update StudentDemo.java

--- C2TC_COREJAVA_ASSIGNMENT/src/entityassignment2/StudentDemo.java
+++ C2TC_COREJAVA_ASSIGNMENT/src/entityassignment2/StudentDemo.java
@@ -3,7 +3,7 @@ package entityassignment2;
 public class StudentDemo {
 	public static void main(String[] args) {
 		Student s=new Student();
-		s.setName("Aswin");
+		s.setName("Mathew");
 		s.setAdress("no: 23,FF Street");
 		s.setPhone(455634241);
 		s.setSales_amount(346788);
